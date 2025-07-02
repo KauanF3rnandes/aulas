93cd3d6d233a1c216809f80d8b1a85e5f3d3fafe
KauanF3rnandes <kauangabrielfp97@gmail.com>
2025-07-02 01:54:29 -0300

    feat: add useRef login page

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,21 +1,20 @@
-import { useCallback, useEffect, useMemo, useState } from "react"
-
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 
 export const Login = () => {
+  const inputPasswordRef = useRef<HTMLInputElement>(null);
 
-    // useState atualiza os valores de algum campo ou algo do tipo
-    const [password, setPassword] = useState('');
-    const [email, setEmail] = useState('');
+  // useState atualiza os valores de algum campo ou algo do tipo
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
 
-    // useMemo ele grava na memória os valores de uma aplicação dentro de uma const ele precisa de 2 parametro 1 - recebe uma função 
-    // 2- array de dependencias colocar parametros de quando atualizar
-    const emailLength = useMemo(() => {
-        console.log('Executou');
-        return email.length * 1000;
-    }, [email.length]);
+  // useMemo ele grava na memória os valores de uma aplicação dentro de uma const ele precisa de 2 parametro 1 - recebe uma função
+  // 2- array de dependencias colocar parametros de quando atualizar
+  const emailLength = useMemo(() => {
+    console.log("Executou");
+    return email.length * 1000;
+  }, [email.length]);
 
-
-    /* useEffect faz o código que está nela executar apenas uma vez, ou se quiser no array de dependencias colocar parametros de quando atualizar
+  /* useEffect faz o código que está nela executar apenas uma vez, ou se quiser no array de dependencias colocar parametros de quando atualizar
     useEffect(() => {
         if (window.confirm('Você é homen')) {
             console.log('Homen');
@@ -25,37 +24,46 @@ export const Login = () => {
     }, []);
     */
 
-    useEffect(() => {
-        console.log(email);
-        console.log(password);
-    }, [email, password]);
-    
-    // useCallback ele guarda o valor em memória e pede  2 parametro 1 - recebe uma função // 2- array de dependencias colocar parametros de quando atualizar
-    // tem a lógica parecida com os outros Hooks acima.
-    const handleEntrar = useCallback(() => {
-        console.log(email);
-        console.log(password);
-    }, [email, password]);
+  useEffect(() => {
+    console.log(email);
+    console.log(password);
+  }, [email, password]);
 
-    return(
-        <div>
-            <form >
-                <p>Quantidade de catacteres no email: {emailLength}</p>
-                <label>
-                    <span>Email</span>
-                    <input value={email} onChange={e => setEmail(e.target.value)} type="email" />
-                </label>
+  // useCallback ele guarda o valor em memória e pede  2 parametro 1 - recebe uma função // 2- array de dependencias colocar parametros de quando atualizar
+  // tem a lógica parecida com os outros Hooks acima.
+  const handleEntrar = useCallback(() => {
+    console.log(email);
+    console.log(password);
+  }, [email, password]);
 
-                <label>
-                    <span>Senha</span>
-                    <input value={password} onChange={s => setPassword(s.target.value)} type="password" />
-                </label>
+  return (
+    <div>
+      <form>
+        <p>Quantidade de catacteres no email: {emailLength}</p>
+        <label>
+          <span>Email</span>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            onKeyDown={e => e.key === 'Enter' ? inputPasswordRef?.current?.focus() : undefined}
+          />
+        </label>
 
-                <button onClick={handleEntrar} type="button">
-                    Entrar
-                </button>
+        <label>
+          <span>Senha</span>
+          <input
+            ref={inputPasswordRef}
+            value={password}
+            onChange={(s) => setPassword(s.target.value)}
+            type="password"
+          />
+        </label>
 
-            </form>
-        </div>
-    )
-}
+        <button onClick={handleEntrar} type="button">
+          Entrar
+        </button>
+      </form>
+    </div>
+  );
+};
