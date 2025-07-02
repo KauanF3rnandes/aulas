c55262f79c343d9bab1e9755f66102b3f5dd27f6
KauanF3rnandes <kauangabrielfp97@gmail.com>
2025-07-01 21:57:16 -0300

    feat: add useEffect login page

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,9 +1,25 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 
 export const Login = () => {
+
+    // useState atualiza os valores de algum campo ou algo do tipo
     const [password, setPassword] = useState('');
     const [email, setEmail] = useState('');
+
+    useEffect(() => {
+        if (window.confirm('Você é homen')) {
+            console.log('Homen');
+        }else{
+            console.log('Mulher');
+        }
+    }, []);
+
+    // useEffect faz o código que está nela executar apenas uma vez, ou se quiser no array de dependencias colocar parametros de quando atualizar
+    useEffect(() => {
+        console.log(email);
+        console.log(password);
+    }, [email, password]);
 
     const handleEntrar = () => {
         console.log(email);
