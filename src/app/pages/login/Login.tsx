4ea943b6926a07c11f1f5a731f7117efeaaeb3b9
KauanF3rnandes <kauangabrielfp97@gmail.com>
2025-07-01 22:14:48 -0300

    feat: add useMemo login page

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useMemo, useState } from "react"
 
 
 export const Login = () => {
@@ -7,6 +7,15 @@ export const Login = () => {
     const [password, setPassword] = useState('');
     const [email, setEmail] = useState('');
 
+    // useMemo ele grava na memória os valores de uma aplicação dentro de uma const ele precisa de 2 parametro 1 - recebe uma função 
+    // 2- array de dependencias colocar parametros de quando atualizar
+    const emailLength = useMemo(() => {
+        console.log('Executou');
+        return email.length * 1000;
+    }, [email.length]);
+
+
+    /* useEffect faz o código que está nela executar apenas uma vez, ou se quiser no array de dependencias colocar parametros de quando atualizar
     useEffect(() => {
         if (window.confirm('Você é homen')) {
             console.log('Homen');
@@ -14,12 +23,13 @@ export const Login = () => {
             console.log('Mulher');
         }
     }, []);
+    */
 
-    // useEffect faz o código que está nela executar apenas uma vez, ou se quiser no array de dependencias colocar parametros de quando atualizar
     useEffect(() => {
         console.log(email);
         console.log(password);
     }, [email, password]);
+    
 
     const handleEntrar = () => {
         console.log(email);
@@ -29,7 +39,7 @@ export const Login = () => {
     return(
         <div>
             <form >
-
+                <p>Quantidade de catacteres no email: {emailLength}</p>
                 <label>
                     <span>Email</span>
                     <input value={email} onChange={e => setEmail(e.target.value)} type="email" />
